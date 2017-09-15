442a26679f268a813fd5b523cb1dae8044be129e
nhatlinhdoan <mnhatlinh.doan@gmail.com>
2017-09-15 13:14:23 +0700

    delete postgresql

--- routes/api/student.js
+++ routes/api/student.js
@@ -5,18 +5,6 @@ var router = express.Router();
 /* GET users listing. */
 router.get('/', function(req, res, next) {
     res.send('respond with a resource');
-    pg.connect(process.env.DATABASE_URL, function (err, client, done) {
-        client.query('SELECT * FROM sinh_vien', function (err, result) {
-            done();
-            if(err){
-                console.error(err);
-                response.send("Error " + err);
-            }
-            else{
-                response.render('pages/db', {result: result.rows});
-            }
-        });
-    });
 });
 
 module.exports = router;
