e1f5426b32b5adbc8a85c590b54b87706226c65d
nhatlinhdoan <mnhatlinh.doan@gmail.com>
2017-09-17 14:47:54 +0700

    react connect to webservice nodejs and display list of students(no layout)

--- client/src/App.js
+++ client/src/App.js
@@ -3,19 +3,37 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    constructor(props) {
+        super(props);
+        this.state = { students: [] }
+    }
+    componentDidMount(){
+        fetch('/api/student', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json',
+            }
+        })
+            .then(res => res.json())
+            .then(students => this.setState({ students }))
+    }
+    render() {
+        return (
+          <div className="App">
+            <div className="App-header">
+              <img src={logo} className="App-logo" alt="logo" />
+              <h2>QUẢN LÝ SINH VIÊN</h2>
+            </div>
+              <h1>Danh sách sinh viên</h1>
+              <br /><br />
+              {this.state.students.length !== 0 ? "" : "api not yet called"}
+              {this.state.students.map(student =>
+                <div key={student.mssv}>{student.mssv} {student.hoten} {student.diemtichluy}</div>
+              )}
+          </div>
+        );
+    }
 }
 
 export default App;
--- routes/api/student.js
+++ routes/api/student.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 var studentModel = require('../../models/studentModel');
 
-/* GET users listing. */
+/* GET student listing. */
 router.get('/:id?', function (req, res, next) {
     if(req.params.id){
         studentModel.getStudentByMssv(req.params.id, function (err, rows) {
@@ -24,6 +24,40 @@ router.get('/:id?', function (req, res, next) {
             }
         });
     }
+});
+
+router.post('/', function (req, res, next) {
+    studentModel.addStudent(req.body, function (err, count) {
+        if(err){
+            res.json(err);
+        }
+        else
+        {
+            res.json(req.body); //or return count 0 or 1
+        }
+    });
+});
+
+router.delete('/delete', function (req, res, next) {
+    studentModel.deleteStudent(req.param.id, function (err, count) {
+        if(err){
+            res.json(err);
+        }
+        else{
+            res.json(count);
+        }
+    });
+});
+
+router.put('/:id', function (req, res, next) {
+    studentModel.updateStudent(req.params.id, req.body, function (err, rows) {
+        if(err){
+            res.json(err);
+        }
+        else{
+            res.json(rows);
+        }
+    });
 });
 
 module.exports = router;
